0f02f8eb767a7075efe630a73dc40f00070f427a
glbrom <golbrm@gmail.com>
2023-08-08 07:34:29 +0300

    no message

--- WeatherTracker/WeatherTracker/Controllers/WeatherViewController.swift
+++ WeatherTracker/WeatherTracker/Controllers/WeatherViewController.swift
@@ -7,17 +7,19 @@
 
 import UIKit
 import CoreLocation
+import Lottie
 
 class WeatherViewController: UIViewController {
-
-    @IBOutlet weak var conditionImageView: UIImageView!
+    
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-    
+    @IBOutlet weak var conditionImageWeatherView: UIImageView!
+    @IBOutlet weak var conditionLottieImageView: LottieAnimationView!
     
     var weatherManager = WeatherManager()
     var locationManager = CLLocationManager()
+    var animationView: LottieAnimationView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +29,10 @@ class WeatherViewController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
         
-        
         weatherManager.delegate = self
         searchTextField.delegate = self
     }
-
-
+    
     @IBAction func searchButtonPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
     }
@@ -40,13 +40,17 @@ class WeatherViewController: UIViewController {
     @IBAction func locationPressed(_ sender: UIButton) {
         locationManager.requestLocation()
     }
+    
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .destructive, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
-
 // MARK: - UITextFieldDelegate
-
 extension WeatherViewController: UITextFieldDelegate {
-    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         searchTextField.endEditing(true)
@@ -58,37 +62,47 @@ extension WeatherViewController: UITextFieldDelegate {
             return true
         } else {
             textField.placeholder = "Enter city name"
+            showAlert(message: "Please. Enter city name")
             return false
         }
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
-        
-       if let city = searchTextField.text {
-           weatherManager.fetchWeather(cityName: city)
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
         }
-        
         searchTextField.text = ""
     }
-    
 }
 
 extension WeatherViewController: WeatherManagerDelegate {
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
         DispatchQueue.main.async {
+            
+            self.animationView?.removeFromSuperview()
+            
+            let animationView = LottieAnimationView(name: weather.conditionNameAnimationWeather)
+            animationView.frame = CGRect(x: 10, y: 0, width: 150, height: 150)
+            animationView.contentMode = .scaleToFill
+            animationView.loopMode = .loop
+            animationView.play()
+            self.conditionLottieImageView.addSubview(animationView)
+            
+            self.animationView = animationView
+            
             self.temperatureLabel.text = weather.temperatureString
-            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.conditionImageWeatherView.image = weather.conditionNameImageWeather
             self.cityLabel.text = weather.cityName
         }
-        
     }
     
     func didFailWithError(error: Error) {
+        DispatchQueue.main.async {
+            self.showAlert(message: "Please. Enter correct city name")
+        }
         print(error)
     }
-    
 }
 
 // MARK: - CLLocationManagerDelegate
