8fe2de8399f9d15382b32f9be68bff4a41942765
glbrom <golbrm@gmail.com>
2023-08-09 21:00:11 +0300

    no message

--- WeatherTracker/WeatherTracker/Model/WeatherModel.swift
+++ WeatherTracker/WeatherTracker/Model/WeatherModel.swift
@@ -6,37 +6,66 @@
 //
 
 import Foundation
+import UIKit
 
 struct WeatherModel {
     let conditionId: Int
     let cityName: String
-//    let feelsLikeTemperature: Double
     let temperature: Double
-    
     
     var temperatureString: String {
         return String(format: "%.1f", temperature)
     }
     
-    var conditionName: String {
+    // lottie animation
+    var conditionNameAnimationWeather: String {
+        let imageName: String
+        
         switch conditionId {
         case 200...232:
-            return "cloud.bolt"
+            imageName = "animation_storm"
         case 300...321:
-            return "cloud.drizzle"
+            imageName = "animation_drizzle"
         case 500...531:
-            return "cloud.rain"
+            imageName = "animation_rain"
         case 600...622:
-            return "cloud.snow"
+            imageName = "animation_snow"
         case 701...781:
-            return "cloud.fog"
+            imageName = "animation_fog"
         case 800:
-            return "sun.max"
+            imageName = "animation_sun"
         case 801...804:
-            return "cloud.bolt"
-        default: return "cloud"
+            imageName = "animation_cloud"
+        default:
+            imageName = "nosign"
         }
+        
+        return imageName
     }
     
-   
+    // image assets
+    var conditionNameImageWeather: UIImage? {
+        let imageName: String
+        
+        switch conditionId {
+        case 200...232:
+            imageName = "storm"
+        case 300...321:
+            imageName = "drizzle"
+        case 500...531:
+            imageName = "rain"
+        case 600...622:
+            imageName = "snow"
+        case 701...781:
+            imageName = "fog"
+        case 800:
+            imageName = "sunny"
+        case 801...804:
+            imageName = "cloud"
+        default:
+            imageName = "choice"
+        }
+        
+        return UIImage(named: imageName)
+    }
 }
